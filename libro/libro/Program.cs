19a861580ee54f58c1248ad717d0542daec4c020
Jorge <jorgesl2002ds3@gmail.com>
2020-11-09 13:20:14 +0100

    hola y adios

--- libro/libro/Program.cs
+++ libro/libro/Program.cs
@@ -7,6 +7,7 @@ namespace libro
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("adios");
            
         }
     }
